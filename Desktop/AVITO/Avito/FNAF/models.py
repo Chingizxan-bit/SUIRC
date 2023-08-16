28b99186b4b554c887462bd70c5efb26c183b1e6
Chingizxan-bit <genadijcert.com>
2023-08-16 17:07:21 +0700

    Исправленный пятый,который раньше четвёртого

--- Desktop/AVITO/Avito/FNAF/models.py
+++ Desktop/AVITO/Avito/FNAF/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+#title-> Заголовок
+#description->описание
+#price->цена
+#created_at->дата создания
+#category->категория
+#author->автор
+#location->локация
+#auction->торг (булевое значение)
+#updated_at-> дата обновления
+#{tupe} + Field -> нейминг типов джанго моделей
+#Модель объявления
+class Advertisement(models.Model):
+    title = models.CharField("Заголовок", max_length=150)
+    category= models.SmallIntegerField("Категория")
+    description = models.TextField("Описание")
+    price = models.DecimalField("Цена", max_digits=10, decimal_places=2)
+    creatied_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    author= models.CharField("Автор", max_length=20)
+    location= models.CharField("Локация", max_length=255)
+    auction = models.BooleanField("Торг", help_text="Отметьте, если торг уместен")
+class Advertisement(models.Model):
+   class Meta: 
+        db_table = 'advertisements' 
+ 
+
+  
--- Desktop/AVITO/Avito/FNAF/views.py
+++ Desktop/AVITO/Avito/FNAF/views.py
@@ -2,8 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+ return render(request, 'index.html')
+ 
+def topSellers(request):
+ return render(request,'top-sellers.html')
    
-   def topSellers(request):
-    return render(request, 'top-sellers.html')
+def register(request):
+ return render(request, 'register.html') 
+ 
+def login(request):
+ return render(request,'login.html')
+ 
+def profile(request):
+ return render(request,'profile.html')
+ 
+
+def advertisement(request):
+ return render(request, 'advertisement.html')
 
